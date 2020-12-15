689cef0297f7a3c10735fbc997a683d27cef3375
Hyunjun Kim <hyunjun5216@naver.com>
2020-12-15 23:53:18 +0900

    movie name & poster props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
 import Movie from './Movie';
 
+const movieTitles = [
+  "Matrix",
+  "Full Metal Jacket",
+  "Oldboy",
+  "Star Wars"
+]
+
+const movieImages = [
+  "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/a/a2/Star_Wars_The_Force_Awakens_Theatrical_Poster.jpg"
+]
+
 function App() {
   return (
     <div className="App">
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
+      <Movie title={movieTitles[0]} poster={movieImages[0]} />
+      <Movie title={movieTitles[1]} poster={movieImages[1]} />
+      <Movie title={movieTitles[2]} poster={movieImages[2]} />
+      <Movie title={movieTitles[3]} poster={movieImages[3]} />
     </div>
   );
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,17 +1,18 @@
 import './Movie.css';
 
-function Movie() {
+function Movie(props) {
+  console.log(props);
   return (
     <div className="Movie">
-      <MoviePoster />
-      <h1>Your name</h1>
+      <MoviePoster poster={props.poster} />
+      <h1>{props.title}</h1>
     </div>
   );
 }
 
-function MoviePoster() {
+function MoviePoster(props) {
   return (
-    <img src="https://upload.wikimedia.org/wikipedia/ko/d/d1/%EB%84%88%EC%9D%98_%EC%9D%B4%EB%A6%84%EC%9D%80.jpg" />
+    <img src={props.poster} alt="poster" />
   )
 }
 
